20fb370dd53b3df66e6d6fd673af0d522943dc7b
Abdiomar Uldana <Abdiumar@list.ru>
2024-12-11 15:11:15 +0500

    Home work 10.12

---
+++ html,css,js/hw.10.12.js
@@ -0,0 +1,14 @@
+const heading = document.querySelector(".title")
+heading.textContent = "HELLO WORLDD";
+
+const aboutDOM = document.querySelector(".infor")
+aboutDOM.textContent = "JavaScript";
+
+const lern = document.querySelector(".Jaksyjak")
+lern.textContent ="Learning";
+
+const interestinginfo = document.querySelector(".FunFact")
+interestinginfo.innerHTML = "Fun Fact";
+
+const name = document.querySelector(".onclick")
+name.textContent = "My name is Uldana"
